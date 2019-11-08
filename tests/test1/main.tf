3df95f36e16a17adb60702851a94f5e42451769f
Danny Smith <danny.smith@rackspace.co.uk>
2019-11-08 22:22:34 +0000

    Update tests

--- tests/test1/main.tf
+++ tests/test1/main.tf
@@ -3,51 +3,75 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+provider "random" {
+  version = "~> 2.0"
+}
+
+locals {
+  tags = {
+    Environment     = "Test"
+    Identifier      = "${random_string.identifier.result}"
+    Purpose         = "Testing aws-terraform-vpc_endpoint"
+    ServiceProvider = "Rackspace"
+    Terraform       = "true"
+  }
+}
+
+resource "random_string" "identifier" {
+  length  = 6
+  special = false
+  lower   = true
+  upper   = false
+  number  = false
+}
+
 module "base_network" {
-  source   = "git@github.com:rackspace-infrastructure-automation/aws-terraform-vpc_basenetwork?ref=master"
-  vpc_name = "VPC-Endpoint-Test"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-vpc_basenetwork?ref=master"
+
+  vpc_name = "VPC-Endpoint-${random_string.identifier.result}"
 }
 
 module "security_groups" {
-  source        = "git@github.com:rackspace-infrastructure-automation/aws-terraform-security_group?ref=v0.0.3"
-  resource_name = "test_sg"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-security_group?ref=v0.0.3"
+
+  resource_name = "${random_string.identifier.result}_sg"
   vpc_id        = "${module.base_network.vpc_id}"
 }
 
 module "vpc_endpoint" {
-  source                                  = "../../module"
-  vpc_id                                  = "${module.base_network.vpc_id}"
-  route_tables_ids_list                   = "${module.base_network.private_route_tables}"
-  security_group_ids_list                 = ["${module.security_groups.vpc_endpoint_security_group_id}"]
-  subnet_ids_list                         = "${module.base_network.private_subnets}"
-  dynamo_db_endpoint_enable               = true
-  s3_endpoint_enable                      = true
+  source = "../../module"
+
   codebuild_endpoint_enable               = true
   codebuild_private_dns_enable            = true
-  codebuild-fips_endpoint_enable          = false
-  codebuild-fips_private_dns_enable       = false
+  codebuild_fips_endpoint_enable          = false
+  codebuild_fips_private_dns_enable       = false
+  dynamo_db_endpoint_enable               = true
   ec2_endpoint_enable                     = true
   ec2_private_dns_enable                  = true
   ec2messages_endpoint_enable             = true
   ec2messages_private_dns_enable          = true
   elasticloadbalancing_endpoint_enable    = true
   elasticloadbalancing_private_dns_enable = true
+  environment                             = "${lookup(local.tags, "Environment")}"
   events_endpoint_enable                  = true
   events_private_dns_enable               = true
-  execute-api_endpoint_enable             = true
-  execute-api_private_dns_enable          = true
-  kinesis-streams_endpoint_enable         = true
-  kinesis-streams_private_dns_enable      = true
+  execute_api_endpoint_enable             = true
+  execute_api_private_dns_enable          = true
+  kinesis_streams_endpoint_enable         = true
+  kinesis_streams_private_dns_enable      = true
   kms_endpoint_enable                     = true
   kms_private_dns_enable                  = true
   logs_endpoint_enable                    = true
   logs_private_dns_enable                 = true
   monitoring_endpoint_enable              = true
   monitoring_private_dns_enable           = true
+  route_tables_ids_list                   = "${module.base_network.private_route_tables}"
+  s3_endpoint_enable                      = true
   sagemaker_runtime_endpoint_enable       = true
   sagemaker_runtime_private_dns_enable    = true
   secretsmanager_endpoint_enable          = true
   secretsmanager_private_dns_enable       = true
+  security_group_ids_list                 = ["${module.security_groups.vpc_endpoint_security_group_id}"]
   servicecatalog_endpoint_enable          = true
   servicecatalog_private_dns_enable       = true
   sns_endpoint_enable                     = true
@@ -56,4 +80,7 @@ module "vpc_endpoint" {
   sqs_private_dns_enable                  = true
   ssm_endpoint_enable                     = true
   ssm_private_dns_enable                  = true
+  subnet_ids_list                         = "${module.base_network.private_subnets}"
+  tags                                    = "${local.tags}"
+  vpc_id                                  = "${module.base_network.vpc_id}"
 }
--- tests/test1/outputs.tf
+++ tests/test1/outputs.tf
@@ -1,11 +1,6 @@
-output "s3_vpc_endpoint_id" {
-  description = "S3 vpc endpoint ID"
-  value       = "${module.vpc_endpoint.s3_vpc_endpoint_id}"
-}
-
-output "dynamodb_vpc_endpoint_id" {
-  description = "S3 dynamodb endpoint ID"
-  value       = "${module.vpc_endpoint.dynamodb_vpc_endpoint_id}"
+output "codebuild_fips_vpc_endpoint_id" {
+  description = "Codebuild-fips vpc endpoint ID"
+  value       = "${module.vpc_endpoint.codebuild_fips_vpc_endpoint_id}"
 }
 
 output "codebuild_vpc_endpoint_id" {
@@ -13,19 +8,19 @@ output "codebuild_vpc_endpoint_id" {
   value       = "${module.vpc_endpoint.codebuild_vpc_endpoint_id}"
 }
 
-output "codebuild-fips_vpc_endpoint_id" {
-  description = "Codebuild-fips vpc endpoint ID"
-  value       = "${module.vpc_endpoint.codebuild-fips_vpc_endpoint_id}"
-}
-
-output "ec2_vpc_endpoint_id" {
-  description = "EC2 vpc endpoint ID"
-  value       = "${module.vpc_endpoint.ec2_vpc_endpoint_id}"
+output "dynamodb_vpc_endpoint_id" {
+  description = "S3 dynamodb endpoint ID"
+  value       = "${module.vpc_endpoint.dynamodb_vpc_endpoint_id}"
 }
 
 output "ec2messages_vpc_endpoint_id" {
   description = "EC2messages vpc endpoint ID"
   value       = "${module.vpc_endpoint.ec2messages_vpc_endpoint_id}"
+}
+
+output "ec2_vpc_endpoint_id" {
+  description = "EC2 vpc endpoint ID"
+  value       = "${module.vpc_endpoint.ec2_vpc_endpoint_id}"
 }
 
 output "elasticloadbalancing_vpc_endpoint_id" {
@@ -38,14 +33,14 @@ output "events_vpc_endpoint_id" {
   value       = "${module.vpc_endpoint.events_vpc_endpoint_id}"
 }
 
-output "execute-api_vpc_endpoint_id" {
+output "execute_api_vpc_endpoint_id" {
   description = "Execute-api vpc endpoint ID"
-  value       = "${module.vpc_endpoint.execute-api_vpc_endpoint_id}"
+  value       = "${module.vpc_endpoint.execute_api_vpc_endpoint_id}"
 }
 
-output "kinesis-streams_vpc_endpoint_id" {
+output "kinesis_streams_vpc_endpoint_id" {
   description = "Kinesis-streams vpc endpoint ID"
-  value       = "${module.vpc_endpoint.kinesis-streams_vpc_endpoint_id}"
+  value       = "${module.vpc_endpoint.kinesis_streams_vpc_endpoint_id}"
 }
 
 output "kms_vpc_endpoint_id" {
@@ -61,6 +56,11 @@ output "logs_vpc_endpoint_id" {
 output "monitoring_vpc_endpoint_id" {
   description = "Monitoring vpc endpoint ID"
   value       = "${module.vpc_endpoint.monitoring_vpc_endpoint_id}"
+}
+
+output "s3_vpc_endpoint_id" {
+  description = "S3 vpc endpoint ID"
+  value       = "${module.vpc_endpoint.s3_vpc_endpoint_id}"
 }
 
 output "sagemaker_runtime_vpc_endpoint_id" {
