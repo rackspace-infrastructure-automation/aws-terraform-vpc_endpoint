e977eeb079a57852b30afcc7c01f670a2bfa7ee8
John Titus <john.titus@rackspace.com>
2019-12-19 12:45:57 -0600

    Sort outputs in alphabetic order

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,11 @@
 output "codebuild_vpc_endpoint_id" {
   description = "CodeBuild VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.codebuild_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.codebuild_endpoint.*.id, [""]), 0)
 }
 
 output "codebuild_fips_vpc_endpoint_id" {
   description = "CodeBuild-fips VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.codebuild_fips_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.codebuild_fips_endpoint.*.id, [""]), 0)
 }
 
 output "dynamodb_vpc_endpoint_id" {
@@ -26,18 +20,12 @@ output "ec2_vpc_endpoint_id" {
 
 output "ec2messages_vpc_endpoint_id" {
   description = "EC2messages VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.ec2messages_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.ec2messages_endpoint.*.id, [""]), 0)
 }
 
 output "elasticloadbalancing_vpc_endpoint_id" {
   description = "Elasticloadbalancing VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.elasticloadbalancing_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.elasticloadbalancing_endpoint.*.id, [""]), 0)
 }
 
 output "events_vpc_endpoint_id" {
@@ -47,18 +35,12 @@ output "events_vpc_endpoint_id" {
 
 output "execute_api_vpc_endpoint_id" {
   description = "Execute-api VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.execute_api_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.execute_api_endpoint.*.id, [""]), 0)
 }
 
 output "kinesis_streams_vpc_endpoint_id" {
   description = "Kinesis-streams VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.kinesis_streams_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.kinesis_streams_endpoint.*.id, [""]), 0)
 }
 
 output "kms_vpc_endpoint_id" {
@@ -73,10 +55,7 @@ output "logs_vpc_endpoint_id" {
 
 output "monitoring_vpc_endpoint_id" {
   description = "Monitoring VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.monitoring_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.monitoring_endpoint.*.id, [""]), 0)
 }
 
 output "s3_vpc_endpoint_id" {
@@ -86,26 +65,17 @@ output "s3_vpc_endpoint_id" {
 
 output "sagemaker_runtime_vpc_endpoint_id" {
   description = "Sagemaker.runtime VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.sagemaker_runtime_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.sagemaker_runtime_endpoint.*.id, [""]), 0)
 }
 
 output "secretsmanager_vpc_endpoint_id" {
   description = "Secretsmanager VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.secretsmanager_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.secretsmanager_endpoint.*.id, [""]), 0)
 }
 
 output "servicecatalog_vpc_endpoint_id" {
   description = "Servicecatalog VPC endpoint ID"
-  value = element(
-    coalescelist(aws_vpc_endpoint.servicecatalog_endpoint.*.id, [""]),
-    0,
-  )
+  value       = element(coalescelist(aws_vpc_endpoint.servicecatalog_endpoint.*.id, [""]), 0)
 }
 
 output "sns_vpc_endpoint_id" {
@@ -122,4 +92,3 @@ output "ssm_vpc_endpoint_id" {
   description = "SSM VPC endpoint ID"
   value       = element(coalescelist(aws_vpc_endpoint.ssm_endpoint.*.id, [""]), 0)
 }
-
